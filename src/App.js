b55f9f565e93904fa2e9932ec62676b868d08897
GuiReis1303 <91472369+GuiReis1303@users.noreply.github.com>
2022-05-31 16:08:19 -0300

    31-05
    - Edição do Menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './Home';
 import Assinatura from './Assinatura';
 import CursosOferecidos from './CursosOferecidos';
+import Cadastro from './Cadastro';
+import Login from './Login';
 
 function App() {
     return (
@@ -11,9 +13,11 @@ function App() {
             <Menu/>
 
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="Assinatura" element={<Assinatura/>} />
-                <Route path="CursosOferecidos" element={<CursosOferecidos/>} />
+                <Route path='/' element={<Home/>} />
+                <Route path='Assinatura' element={<Assinatura/>} />
+                <Route path='CursosOferecidos' element={<CursosOferecidos/>} />
+                <Route path='Cadastro' element={<Cadastro/>} />
+                <Route path='Login' element={<Login/>} /> 
             </Routes>
         </div>
   );
--- src/Menu.js
+++ src/Menu.js
@@ -5,10 +5,17 @@ const Menu = () => {
     require('./Menu.css');
     return ( 
         <div className="menu">
-            <div className="itemsmenu">
+            <div className="imagem">
+                <img src="https://www.jotform.com/pt/resources/assets/logo-nb/jotform-logo-dark-400x200.png" />
+            </div>
+            <div className="link">
                 <Link to="/" style={{ textdecoration: 'none'}}>Home</Link> | 
                 <Link to="/Assinatura">Assinatura</Link> |
                 <Link to="/CursosOferecidos">Cursos Oferecidos</Link>
+            </div>
+            <div className="login">
+                <Link to="/Cadastro">Cadastre-se</Link> |
+                <Link to="/Login">Login</Link>
             </div>
             
         </div>
