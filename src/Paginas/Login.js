a9dcf88e66f822a0c712e88fe74ecb579382d785
GuiReis1303 <91472369+GuiReis1303@users.noreply.github.com>
2022-06-03 14:06:07 -0300

    03-06
    - Edição do Login.js
    - Adição do Login.css

--- src/Paginas/Login.js
+++ src/Paginas/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    require('./Login.css')
     return ( 
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <div className="caixaLogin">
+                <form>
+                    <input type="nick" placeholder="Email ou Nick"></input>
+                    <input type="password" placeholder="Senha"></input>
+
+                    <button type="submit" className="">Enviar</button>
+                    <Link to={"/"}>
+                        <button type="cancel" className="">Cancelar</button>
+                    </Link>
+                </form>
+            </div>
         </div>
      );
 }
