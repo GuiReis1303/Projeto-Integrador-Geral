b58e7e396deed834a607b77a3ea7bf846cd269c5
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2022-06-24 22:55:11 -0300

    24/06

--- src/Paginas/SemAcesso.js
+++ src/Paginas/SemAcesso.js
@@ -2,26 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SemAcesso = () => {
-    require('./SemAcesso.css')
+    require('../Styles/SemAcesso.css')
     return ( 
-        <div className="semacesso">
+        <div>
 
+        <div className="semacesso">
+          
             <div className="semacesso-right">
                 <img className="caixacursos-carouselImagem" src={require("./Imagens/semacesso.png")}/>
                 </div>
             <div className="semacesso-left">
                 <div className="semacesso-left-texto">
-                    <h2> Parece que você não tem acesso para ver isso!</h2>
-                    <p>Logue para conseguir acessar essa página</p>
+                    <h2>Você precisa estar logado para acessar essa página.</h2>
+                   
                     
                     <div className="semacesso-left-botao">
                         <Link to={'/'}><button>Voltar</button></Link>
-                        <Link to={'/Login2'}><button>Login</button></Link>
+                        <Link to={'/Login2'}><button id="blsemacesso">Login</button></Link>
+                      
                     </div>
+                    <div className="semacesso-cadastro">
+                <h2> Não possui conta?<Link className='text-link' to="/Cadastro"><b> Cadastre-se</b> </Link>  agora!</h2>
+                <Link to={'/Cadastro'}><button>Cadastro</button></Link>
                 </div>
-                
+                </div>
+               
             </div>
+            
         </div>
+     </div>
      );
 }
  
