6303bd7495314a70db292477ece7dc844c600025
GuiReis1303 <91472369+GuiReis1303@users.noreply.github.com>
2022-06-28 10:33:20 -0300

    .
    .

--- stationclass.sql
+++ stationclass.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `stationclass` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `stationclass`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: stationclass
@@ -164,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-28 10:29:26
+-- Dump completed on 2022-06-28 10:32:53
