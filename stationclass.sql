af2729cbf97dcbe3827216bab2fb8c140ec35967
GuiReis1303 <91472369+GuiReis1303@users.noreply.github.com>
2022-06-28 10:30:15 -0300

    28-06
    - Adição do Arquivo stationclass.sql

--- stationclass.sql
+++ stationclass.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `stationclass` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `stationclass`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: stationclass
@@ -28,11 +26,9 @@ CREATE TABLE `aula` (
   `idaula` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
   `urlvideo` varchar(60) NOT NULL,
-  `avaliacaoPositiva` int(11) NOT NULL,
-  `avaliacaoNegativa` int(11) NOT NULL,
   PRIMARY KEY (`idaula`),
   UNIQUE KEY `idaula_UNIQUE` (`idaula`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,38 +37,8 @@ CREATE TABLE `aula` (
 
 LOCK TABLES `aula` WRITE;
 /*!40000 ALTER TABLE `aula` DISABLE KEYS */;
+INSERT INTO `aula` VALUES (1,'Criando a Estrutura HTML','https://www.youtube-nocookie.com/embed/uCyVx1vYDz8'),(2,'Como Começar a Investir','https://www.youtube-nocookie.com/embed/uCyVx1vYDz8'),(3,'Aprendendo Photoshop','https://www.youtube-nocookie.com/embed/uCyVx1vYDz8');
 /*!40000 ALTER TABLE `aula` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `comentario`
---
-
-DROP TABLE IF EXISTS `comentario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comentario` (
-  `idcomentario` int(11) NOT NULL AUTO_INCREMENT,
-  `comentario` mediumtext CHARACTER SET latin1 NOT NULL,
-  `registro` datetime NOT NULL,
-  `iduser` int(11) NOT NULL,
-  `idaula` int(11) NOT NULL,
-  PRIMARY KEY (`idcomentario`),
-  UNIQUE KEY `idcomentario_UNIQUE` (`idcomentario`),
-  KEY `iduser_idx` (`iduser`),
-  KEY `idaula_idx` (`idaula`),
-  CONSTRAINT `idaula_comentario` FOREIGN KEY (`idaula`) REFERENCES `aula` (`idaula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `iduser_comentario` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comentario`
---
-
-LOCK TABLES `comentario` WRITE;
-/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,7 +57,7 @@ CREATE TABLE `comunidade` (
   PRIMARY KEY (`idcomunidade`),
   UNIQUE KEY `idcomunidade_UNIQUE` (`idcomunidade`),
   KEY `iduser_idx` (`iduser`),
-  CONSTRAINT `iduser_comunidade` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `iduser_comunidade` FOREIGN KEY (`iduser`) REFERENCES `usuario` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -114,12 +80,10 @@ DROP TABLE IF EXISTS `curso`;
 CREATE TABLE `curso` (
   `idcurso` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) CHARACTER SET latin1 NOT NULL,
-  `descricao` mediumtext CHARACTER SET latin1 NOT NULL,
-  `avaliacaoPositiva` int(11) NOT NULL,
-  `avaliacaoNegativa` int(11) NOT NULL,
+  `idavaliacao` int(11) DEFAULT NULL,
   PRIMARY KEY (`idcurso`),
   UNIQUE KEY `idcurso_UNIQUE` (`idcurso`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,44 +92,48 @@ CREATE TABLE `curso` (
 
 LOCK TABLES `curso` WRITE;
 /*!40000 ALTER TABLE `curso` DISABLE KEYS */;
+INSERT INTO `curso` VALUES (1,'HTML Completo',NULL);
 /*!40000 ALTER TABLE `curso` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `modulos`
+-- Table structure for table `curtida_aula`
 --
 
-DROP TABLE IF EXISTS `modulos`;
+DROP TABLE IF EXISTS `curtida_aula`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `modulos` (
-  `idmodulos` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  `idcurso` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idmodulos`),
-  UNIQUE KEY `idmodulos_UNIQUE` (`idmodulos`),
-  KEY `idcurso_idx` (`idcurso`),
-  CONSTRAINT `idcurso_modulos` FOREIGN KEY (`idcurso`) REFERENCES `curso` (`idcurso`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `curtida_aula` (
+  `idcurtida_aula` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `idaula` int(11) NOT NULL,
+  PRIMARY KEY (`idcurtida_aula`),
+  UNIQUE KEY `idcurtida_aula_UNIQUE` (`idcurtida_aula`),
+  KEY `iduser_idx` (`iduser`),
+  KEY `idaula_idx` (`idaula`),
+  CONSTRAINT `idaula` FOREIGN KEY (`idaula`) REFERENCES `aula` (`idaula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `iduser` FOREIGN KEY (`iduser`) REFERENCES `usuario` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `modulos`
+-- Dumping data for table `curtida_aula`
 --
 
-LOCK TABLES `modulos` WRITE;
-/*!40000 ALTER TABLE `modulos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `modulos` ENABLE KEYS */;
+LOCK TABLES `curtida_aula` WRITE;
+/*!40000 ALTER TABLE `curtida_aula` DISABLE KEYS */;
+INSERT INTO `curtida_aula` VALUES (19,1,1),(20,1,2),(21,1,3),(22,2,1),(23,2,2),(24,2,3),(25,3,1),(26,3,2),(27,3,3),(28,4,1),(29,4,2),(30,4,3),(31,5,1),(32,5,2),(33,5,3),(34,6,1),(35,6,2),(36,6,3),(37,1,1),(38,1,1);
+/*!40000 ALTER TABLE `curtida_aula` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE `usuario` (
   `iduser` int(11) NOT NULL AUTO_INCREMENT,
   `nick` varchar(30) NOT NULL,
   `email` varchar(60) NOT NULL,
@@ -174,16 +142,17 @@ CREATE TABLE `user` (
   UNIQUE KEY `iduser_UNIQUE` (`iduser`),
   UNIQUE KEY `nick_UNIQUE` (`nick`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `usuario`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'gui','gui@gmail.com','12345'),(2,'Conrado','conrado@gmail.com','12345'),(3,'Paulo','paulo@gmail.com','12345'),(4,'Marcos','marcos@gmail.com','12345'),(5,'Aralto','aralto@gmail.com','12345'),(6,'gui1','gui','12345'),(7,'','',''),(10,'123','gui12','13245'),(11,'sla13','sla@gmail.com','12345'),(13,'teste','teste','teste'),(14,'teste1','teste1','teste1'),(15,'teste2','teste2','teste2'),(16,'teste3','teste3','teste3'),(17,'teste4','teste4','teste4'),(18,'testes2','testes1','testes2'),(22,'134012g','gui13@gmail.com','12345'),(23,'Gui14','gui14@gmail.com','12345'),(24,'isa ','isabelaap@gmail.com','1234'),(25,'reis13','reis@gmail.com','12345'),(27,'reis14','reis14@gmail.com','12345'),(28,'reis15','reis15@gmail.com','12345'),(29,'sirley13','sirley@gmail.com','12345'),(30,'bia13','bia@gmail.com','12345'),(31,'sally13','sally@gmail.com','12345'),(32,'sally14','sally14@gmail.com','12345'),(33,'marcos13','marcos13@gmail.com','12345'),(34,'sasasass','guireis1303@gmail.comsss','sssssssssssss'),(35,'felipe13','felipe@gmail.com','12345');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -195,4 +164,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-14 11:51:17
+-- Dump completed on 2022-06-28 10:29:26
